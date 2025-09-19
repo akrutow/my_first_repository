6f1037f0e1fd78624e11478704d853c0cb4d0caa
Artem Krutov <aikrutow@gmail.com>
2025-09-19 23:25:04 +0300

    Добавлена функция деления

--- first/nums.py
+++ first/nums.py
@@ -6,6 +6,10 @@ def minus(a, b):
     return a - b
 
 
+def delenye(a, b):
+    return a / b
+
+
 def multy(a, b):
     return a * b
 
@@ -14,3 +18,4 @@ if __name__ == '__main__':
     print(plus(2, 2))
     print(minus(5, 2))
     print(multy(3, 3))
+    print(delenye(12, 3))
